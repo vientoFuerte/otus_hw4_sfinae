c5c21f0d1a9f3429a9e1ad890f5eccf2db0a4a0f
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-12-13 18:21:54 +0300

    Update main.cpp
    Добавлен вывод на печать разных типов int.

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@
 int main()
 {
 
-print_ip( int8_t{-1} ); // 255 
-
-#if 0
-print_ip( int16_t{0} ); // 0.0 
+    print_ip( int8_t{-1} ); // 255 
+    print_ip( int16_t{0} ); // 0.0 
     print_ip( int32_t{2130706433} ); // 127.0.0.1 
     print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41 
+    
+#if 0
     print_ip( std::string{“Hello, World!”} ); // Hello, World! 
     print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 
     print_ip( std::list<shot>{400, 300, 200, 100} ); // 400.300.200.100 
@@ -23,6 +23,7 @@ print_ip( int16_t{0} ); // 0.0
 #endif
     return 0;
 }
+
 
 
 
