7ad3fc69779c2222a476b216a4a9864e1564e277
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-12-14 20:24:37 +0300

    Update main.cpp
    Раскомментирована печать для кортежа.

--- main.cpp
+++ main.cpp
@@ -16,10 +16,11 @@ int main()
     print_ip( std::string{“Hello, World!”} ); // Hello, World! 
     print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 
     print_ip( std::list<shot>{400, 300, 200, 100} ); // 400.300.200.100 
-    //print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+    print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
 
     return 0;
 }
+
 
 
 
