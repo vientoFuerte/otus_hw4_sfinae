d367111acfedd8e3b87d9c4f5bd8f97d0b39d1e7
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-12-14 14:14:21 +0300

    Update print_ip.h
    Добавлен вывод для  vector<int>

--- print_ip.h
+++ print_ip.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <iostream>
 #include <cstring>  
+#include <vector>
 
 template<typename T>
 struct type_is{
@@ -17,9 +18,9 @@ struct is_int {
 };
 
 
-// is_int <int>::value вернет true
+// is_int <int_32>::value вернет true
 template<>
-struct is_int <int>{
+struct is_int <int32_t>{
 	static const bool value = true;
 };
 
@@ -35,11 +36,25 @@ struct is_int <int16_t>{
     static const bool value = true;
 };
 
+
 // Добавляем специализацию для int64_t
 template<>
 struct is_int <int64_t>{
     static const bool value = true;
 };
+
+
+// Проверка на vector<int>
+template<typename T>
+struct is_vector_int {
+    static const bool value = false;
+};
+
+template<>
+struct is_vector_int<std::vector<int>> {
+    static const bool value = true;
+};
+
 
 template <bool C, class T=void>
 struct enable_if: type_is<T> {};
@@ -53,7 +68,7 @@ template<typename T,
          typename = typename enable_if<is_int<T>::value>::type>
 void print_ip(T ip) {
     // получение байтов
-     unsigned char bytes[sizeof(T)];
+    unsigned char bytes[sizeof(T)];
     std::memcpy(bytes, &ip, sizeof(T));
     for (size_t i = sizeof(T); i > 0; --i) {
         std::cout << static_cast<unsigned int>(bytes[i - 1]);
@@ -62,10 +77,24 @@ void print_ip(T ip) {
     std::cout << std::endl;
 }
 
-// Для std::string
+
+// Для std::string 
 template<typename T,
          typename = typename enable_if<std::is_same<T, std::string>::value>::type>
-void print_ip(const T &ip) {
+void print_ip(const T &ip, int=0) {
 
         std::cout << ip<< std::endl;
 }
+
+// Для vector<int> (Использован дополнительный фиктивный параметр, чтобы отличать от версии std::string)
+template<typename T,
+     typename = typename enable_if<is_vector_int<T>::value>::type>
+void print_ip(const T &ip, long=0) {
+
+	for (size_t i=0; i<ip.size()-1; i++)
+	{
+    	std::cout <<ip[i]<< ".";
+    }
+	std::cout<<ip[ip.size()-1]<< std::endl;
+}
+
