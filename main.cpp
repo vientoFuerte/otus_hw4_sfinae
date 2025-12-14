ee4c5bc03a625dcdb561cf96c730052f3dc367b9
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-12-14 20:27:00 +0300

    Update main.cpp
    Корректные кавычки при выводе строки.

--- main.cpp
+++ main.cpp
@@ -13,13 +13,14 @@ int main()
     print_ip( int32_t{2130706433} ); // 127.0.0.1 
     print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41 
 
-    print_ip( std::string{“Hello, World!”} ); // Hello, World! 
+    print_ip( std::string{"Hello, World!"} ); // Hello, World! 
     print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 
     print_ip( std::list<shot>{400, 300, 200, 100} ); // 400.300.200.100 
     print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
 
     return 0;
 }
+
 
 
 
