eb79bf549f34deafab0cd1161e7a9b8f8392cf1f
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-12-14 20:28:53 +0300

    Update main.cpp
    Исправлена ошибка в типе данных.

--- main.cpp
+++ main.cpp
@@ -15,11 +15,12 @@ int main()
 
     print_ip( std::string{"Hello, World!"} ); // Hello, World! 
     print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 
-    print_ip( std::list<shot>{400, 300, 200, 100} ); // 400.300.200.100 
+    print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100 
     print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
 
     return 0;
 }
+
 
 
 
